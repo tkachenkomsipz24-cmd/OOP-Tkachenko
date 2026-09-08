df53a9a47110570501b38d8fb5e648cb2bfa5742
tByte97 <makssusuk@gmail.com>
2026-09-08 17:25:39 +0300

    Lab1: перейменовано папку на lab1v5

--- lab1v1/Program.cs
+++ lab1v1/Program.cs
@@ -2,17 +2,17 @@
 
 namespace lab1v5
 {
-    // Клас за варіантом 5: City
+    
     public class City
     {
-        // Приватні поля
+       
         private string name;
         private string country;
 
-        // Публічна властивість
+        
         public int Population { get; set; }
 
-        // Конструктор для ініціалізації
+       
         public City(string name, string country, int population)
         {
             this.name = name;
@@ -20,13 +20,13 @@ namespace lab1v5
             Population = population;
         }
 
-        // Деструктор
+       
         ~City()
         {
             Console.WriteLine($"[Деструктор] Об'єкт міста \"{name}\" видалено.");
         }
 
-        // Метод для отримання інформації
+        
         public string GetInfo()
         {
             return $"Місто: {name} | Країна: {country} | Населення: {Population} осіб";
@@ -42,7 +42,7 @@ namespace lab1v5
             City city2 = new City("Токіо", "Японія", 13960000);
             City city3 = new City("Париж", "Франція", 2161000);
 
-            // Виклик методу GetInfo та вивід у консоль
+           
             Console.WriteLine(city1.GetInfo());
             Console.WriteLine(city2.GetInfo());
             Console.WriteLine(city3.GetInfo());
